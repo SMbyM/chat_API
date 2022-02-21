7b34a99eb86990d1abec7d96191c3e145b89e5bc
SemyonC20 <semenmalahov906@gmail.com>
2022-02-21 21:14:07 +0700

    One

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
-
-
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-        return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
 
 
 @app.route('/')
@@ -24,4 +9,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
